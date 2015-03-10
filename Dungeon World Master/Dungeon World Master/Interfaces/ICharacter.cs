9d80c4b35d1896d56ed4d75ee2a803b5c05cd0b1
Marcus Haagh <haagh9@hotmail.com>
2015-03-10 18:09:19 +0100

    Basic concept interfaces

---
+++ Dungeon World Master/Dungeon World Master/Interfaces/ICharacter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon_World_Master.Interfaces {
+    public interface ICharacter {
+        string Name { get; set; }
+        IStat[] Stats { get; }
+        int Level { get; set; }
+        string Race { get; set; }
+        string Class { get; }
+        string Alignment { get; set; }
+        List<string> Notes { get; }
+    }
+}
